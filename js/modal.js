34de124b12b18d640205ab3e9c0214bc92fcbb6b
felipe coelho <felipeaacoelho@gmail.com>
2023-02-15 00:10:14 -0300

    first upload

---
+++ js/modal.js
@@ -0,0 +1,63 @@
+export const Modal = {
+
+    intervalHandle: "",
+
+    icons: document.querySelector(".icon-buttons"),
+
+    timer: document.querySelector("#timer"),
+
+    buttonPlay: document.querySelector(".play"),
+    buttonPause: document.querySelector(".pause"),
+
+    setTime: document.querySelector(".set"),
+    stopCircle: document.querySelector(".stop"),
+
+    volumeMedium: document.querySelector(".medium"),
+    volumeMute: document.querySelector(".mute"),
+
+    minutes: document.querySelector("#minutes"),
+    seconds: document.querySelector("#seconds"),
+
+    music: document.querySelector(".music"),
+    alarm: document.querySelector(".alarm"),
+
+    toggle(element1, element2){
+        element1.classList.toggle("hide")
+        element2.classList.toggle("hide")
+    },
+
+    startTimer(duration) {
+        var interval = duration, getMinutes, getSeconds;
+        
+        Modal.intervalHandle = setInterval(function () {
+            
+            getMinutes = parseInt(interval / 60, 10);
+            getSeconds = parseInt(interval % 60, 10);
+    
+            getMinutes = Modal.checkValueTimer(getMinutes)
+            getSeconds = Modal.checkValueTimer(getSeconds)
+    
+            Modal.minutes.textContent = getMinutes;
+            Modal.seconds.textContent = getSeconds;
+    
+            if (--interval < 0) {
+                clearInterval(Modal.intervalHandle)
+                Modal.music.pause()
+
+                var timeOut = 15
+                var alarmHadle = setInterval(() => {
+                    Modal.alarm.play()
+                    if(--timeOut < 0){
+                        clearInterval(alarmHadle)
+                        Modal.alarm.pause()
+                    }
+                }, 250);             
+            }
+        }, 1000);
+    },
+
+    checkValueTimer(time){
+        return time < 10 ? "0" + time : time;
+    }
+}
+
---
+++ js/script.js
@@ -0,0 +1,70 @@
+import { Modal } from "./modal.js";
+
+const minutes = Modal.minutes
+const seconds = Modal.seconds
+
+let timer = 0;
+
+Modal.buttonPlay.addEventListener("click", ()=>{
+
+    let min = Number(minutes.innerText)
+    let sec = Number(seconds.innerText)
+    timer = min * 60 + sec
+
+    if(min != 0 || sec != 0){
+
+        Modal.startTimer(timer)
+    
+        Modal.toggle(Modal.buttonPlay, Modal.buttonPause)
+
+        Modal.music.play()
+        Modal.music.loop = true;
+
+    }
+
+})
+Modal.buttonPause.addEventListener("click", ()=>{
+
+    clearInterval(Modal.intervalHandle);
+    Modal.music.pause()
+    Modal.toggle(Modal.buttonPlay, Modal.buttonPause)
+
+})
+Modal.setTime.addEventListener("click", ()=>{
+
+    const setMinutes = Number(prompt("Minutos: "))
+    const setSeconds = Number(prompt("Segundos: "))
+
+    if(setMinutes != 0 || setSeconds != 0){
+        minutes.innerText = Modal.checkValueTimer(setMinutes)
+        seconds.innerText = Modal.checkValueTimer(setSeconds)
+
+        Modal.toggle(Modal.setTime, Modal.stopCircle)
+    }
+
+})
+
+Modal.stopCircle.addEventListener("click", ()=>{
+
+    clearInterval(Modal.intervalHandle);
+
+    minutes.innerText = "00"
+    seconds.innerText = "00"
+
+    Modal.music.pause()
+    Modal.toggle(Modal.setTime, Modal.stopCircle)
+
+    if(Modal.buttonPlay.classList.contains('hide'))
+        Modal.toggle(Modal.buttonPlay, Modal.buttonPause)
+    
+})
+Modal.volumeMedium.addEventListener("click", ()=>{
+    
+    Modal.music.volume = 0
+    Modal.toggle(Modal.volumeMedium, Modal.volumeMute)
+})
+Modal.volumeMute.addEventListener("click", ()=>{
+
+    Modal.music.volume = 1;
+    Modal.toggle(Modal.volumeMedium, Modal.volumeMute)
+})
